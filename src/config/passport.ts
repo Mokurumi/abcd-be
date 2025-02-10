d04fe3a02864b36b7c21c76e95fcce7d62a584e4
Alpha Mokurumi <mokurumialpha@gmail.com>
2025-02-10 06:56:51 +0300

    Update passport.ts

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,7 +1,7 @@
 import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
 import config from ".";
 import { tokenTypes } from "../constants";
-// import { userService } from "../services";
+import { userService } from "../services";
 
 const jwtOptions = {
   secretOrKey: config.jwt.secret,
@@ -13,11 +13,11 @@ const jwtVerify = async (payload: any, done: any) => {
     if (payload.type !== tokenTypes.ACCESS) {
       throw new Error("Invalid token type");
     }
-    // const user = await userService.getUserById(payload.sub);
-    // if (!user) {
-    return done(null, false);
-    // }
-    // done(null, user);
+    const user = await userService.getUserById(payload.sub);
+    if (!user) {
+      return done(null, false);
+    }
+    done(null, user);
   } catch (error) {
     done(error, false);
   }
